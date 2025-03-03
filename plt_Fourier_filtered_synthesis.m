1e0e8d234273b7a75121b93d3782b9a1b16864f7
pdms_ibs_brilliantPC <vjavmvja@gmail.com>
2025-03-03 21:05:29 +0900

    1st Migration

---
+++ plt_Fourier_filtered_synthesis.m
@@ -0,0 +1,64 @@
+function plt_Fourier_filtered_synthesis(Z,bin)
+
+if ~isequal(size(Z), size(bin))
+    error('Input vectors v1 and v2 must have the same size.');
+end
+fZ = fft2(Z);
+
+f_maskedFFT = figure(3);
+f= f_maskedFFT;
+msb = 2;
+nsb = 3;
+p = 0;
+for isb = 1:msb
+    for jsb = 1:nsb
+        p = p + 1;
+        sbs(isb,jsb)=subplot(msb,nsb,p)
+    end
+end
+sgtitle('filtered FFT')
+
+% real space raw
+ax_raw_real = sbs(1,1);
+f.CurrentAxes = ax_raw_real;
+views(Z)
+colormap(gray)
+titlet('real space')
+
+% reciprocal space
+f.CurrentAxes = sbs(1,2)
+views(abs(fftshift(fZ)))
+climsc([0 .001]).Minmax
+titlet('FFT')
+
+% fftMask
+f.CurrentAxes = sbs(1,3)
+% bin = ~ftCirc(size(Z),30)&ftCirc(size(Z),60)
+views(fftshift(bin))
+titlet('FFT mask')
+
+% maksedFFT
+f.CurrentAxes = sbs(2,3)
+mskedFFT = fZ.*bin;
+abmskedFFT = abs(mskedFFT)
+viewst(abmskedFFT)
+sbs(2,3).CLim = sbs(1,2).CLim
+
+
+f.CurrentAxes = sbs(2,2)
+ffiltZ = ifft2(mskedFFT)
+views(ffiltZ)
+
+f.CurrentAxes = sbs(2,1)
+views(Z-ffiltZ)
+end
+
+function titlet(str)
+title(str)
+ax = gca;
+ax.Tag = str;
+end
+
+function viewst(img)
+views(fftshift(img))
+end
---
+++ testGihub.m
@@ -0,0 +1,16 @@
+%[text:table]{"ignoreHeader":true}
+%[text] |  |  |  |
+%[text] | --- | --- | --- |
+%[text] |  |  |  |
+%[text:table]
+%[text] 
+'Hello github' %[output:708f23a9]
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"onright"}
+%---
+%[output:708f23a9]
+%   data: {"dataType":"textualVariable","outputData":{"name":"ans","value":"'Hello github'"}}
+%---
