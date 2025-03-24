395c01d0b5f085bdca1e88ed503843205e502d52
pdms_ibs_brilliantPC <vjavmvja@gmail.com>
2025-03-24 20:34:00 +0900

    update Dr.Lee figure 1) clim 2) SoD

---
+++ ldos_conductance_Together__Constants.m
@@ -0,0 +1,13 @@
+xyxyr = [50 300 1000 200 100];
+vFcss = [-600 -300 -60 0 130 400 500;
+    -580 -350 -160 0 50 250 550];
+thtSoD = -pi/2 + atan(sqrt(3)/7);
+sclSoD = 4.2;
+cnrSoDs{1} = [20.4 32.2];% cell index indicate phase
+cnrSoDs{2} = [31 37];
+
+%[appendix]{"version":"1.0"}
+%---
+%[metadata:view]
+%   data: {"layout":"onright"}
+%---
